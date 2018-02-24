499ecdef9ebbcdb41e65c2cb7d7ab1703de5f01b
Alex Beal <alexlbeal@gmail.com>
2018-02-24 09:49:05 -0700

    Add Rot instructions and implement multiply program.

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,7 @@ import Foundation.Collection
 import qualified Data.Map.Strict as M
 import qualified Data.Bits as B
 import qualified Data.Tree as T
+import qualified Data.SBV as S
 
 import Prelude (getLine, read)
 
@@ -30,7 +31,8 @@ data Instr = Add
            | Swap
            | Dup
            | Over
-           | Rot
+           | RotL
+           | RotR
            | Done deriving (Eq, Show)
 
 -- | A program is a list of instructions.
@@ -45,7 +47,7 @@ type State = (Int, Mem, [Word32])
 run :: Bool -> Prog -> State-> IO [Word32]
 run trace prg st@(pc, _, stack) = do
   let Just instr = (prg ! (Offset pc))
-  when trace $ putStrLn $ "Trace: " <> (show pc) <> " " <> (show instr)
+  when trace $ putStrLn $ "Trace: " <> (show pc) <> " " <> (show instr) <> " " <> (show stack)
   if instr == Done
     then return stack
     else step st instr >>= run trace prg
@@ -69,7 +71,7 @@ step (pc, mem, l:r:stack) Or =
     return (pc+1, mem, res:stack)
 step (pc, mem, a:stack) Not =
   let a' = wordToBool a
-      res = boolToWord a'
+      res = boolToWord (not a')
   in
     return (pc+1, mem, res:stack)  
 step (pc, mem, l:r:stack) Lt =
@@ -108,7 +110,7 @@ step (pc, mem, w:stack) Dup =
   return (pc+1, mem, w:w:stack)
 step (pc, mem, w:stack) Over =
   return (pc+1, mem, w:stack <> [w])
-step (pc, mem, w:stack) Rot =
+step (pc, mem, w:stack) RotL =
   return (pc+1, mem, stack <> [w])
 step _ Done =
   error "No state transition for Done!!"
@@ -146,7 +148,7 @@ data Constraint = CAdd Constraint Constraint
 renderConstraint :: Constraint -> String
 renderConstraint (CAdd l r) = renderConstraint l <> " + " <> renderConstraint r
 renderConstraint (CCon w) = show (wordToSignedInt w)
-renderConstraint (CAny i) = "var_" <> (show i)
+renderConstraint (CAny i) = "val_" <> (show i)
 renderConstraint (CEq l r) = renderConstraint l <> " = " <> renderConstraint r
 renderConstraint (CNot c) = "~(" <> renderConstraint c <> ")"
 
@@ -167,6 +169,7 @@ symStep :: SymState -> Instr -> [SymState]
 symStep (pc, i, mem, l:r:stack, cs) Add = pure (pc+1, i, mem, CAdd l r : stack, cs)
 symStep (pc, i, mem, stack, cs) Read = pure (pc+1, i+1, mem, CAny i : stack, cs)
 symStep (pc, i, mem, stack, cs) (Push w) = pure (pc+1, i, mem, CCon w : stack, cs)
+symStep (pc, i, mem, _:stack, cs) Pop = pure (pc+1, i, mem, stack, cs)
 symStep (pc, i, mem, w:stack, cs) Dup = pure (pc+1, i, mem, w:w:stack, cs)
 symStep (pc, i, mem, _:stack, cs) Print = pure (pc+1, i, mem, stack, cs)
 symStep (pc, i, mem, x:y:stack, cs) Swap = pure (pc+1, i, mem, y:x:stack, cs)
@@ -178,9 +181,9 @@ symStep (pc, i, mem, _:_:stack, cs) JmpIf =
   -- If the jump address is not concrete, don't explore that branch
   -- The jump could be to anywhere in the program.
   pure (pc+1, i, mem, stack, cs)
-symStep (pc, i, mem, _:stack, cs) Pop = pure (pc+1, i, mem, stack, cs)
 symStep (pc, i, mem, w:stack, cs) Over = pure (pc+1, i, mem, w:stack <> [w], cs)
-symStep (pc, i, mem, w:stack, cs) Rot = pure (pc+1, i, mem, stack <> [w], cs)
+symStep (pc, i, mem, w:stack, cs) RotL = pure (pc+1, i, mem, stack <> [w], cs)
+symStep (pc, i, mem, w:stack, cs) Not = pure (pc+1, i, mem, CNot w:stack, cs)
 symStep _ Done = error "No step for Done"
 
 defaultSymState :: SymState
@@ -190,12 +193,12 @@ main :: IO ()
 main = do
   args <- getArgs
   let trace = elem "-t" args
-  let prog = listToProgram countDown
+  let prog = listToProgram multiply
   putStrLn $ show prog
   stack <- run trace prog (0, M.empty, [])
   putStrLn $ show $ wordToSignedInt <$> stack
 
-  let traces = symbolic 20 prog defaultSymState
+  let traces = symbolic 25 prog defaultSymState
   putStrLn $ fromString $ T.drawTree $ fmap (toList . show . \(pc,_,_,st,cs) -> (pc, renderConstraint <$> st, renderConstraint <$> cs)) traces
 
 countDown :: [Instr]
@@ -211,6 +214,30 @@ countDown = [ Read
             , Pop
             , Done
             ]
+
+multiply :: [Instr]
+multiply = [ Read
+           , Dup
+           , Read
+           , Push (twosComplement 1) -- start
+           , Add
+           , Dup
+           , Not
+           , Push 18 -- end address
+           , Swap
+           , JmpIf
+           , RotL
+           , Dup
+           , RotL
+           , Add
+           , RotL
+           , Push 3 -- start address
+           , Push 1
+           , JmpIf
+           , Pop --end
+           , Pop
+           , Print
+           , Done ]
 
 addInputs :: [Instr]
 addInputs = [ Read
